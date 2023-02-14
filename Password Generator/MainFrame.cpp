3eed8b0487e4ee8e13e10e2afc90de9f02135e42
darksnakepy <darkhuntshowdown@gmail.com>
2023-02-14 22:19:12 +0100

    made the random pass and request function

--- Password Generator/MainFrame.cpp
+++ Password Generator/MainFrame.cpp
@@ -1,6 +1,9 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 #include "app.h"
+#include <stdexcept>
+#include <curl/curl.h>
+
 
 enum IDs {
 	BUTTON_ID = 2
@@ -11,19 +14,38 @@ EVT_BUTTON(BUTTON_ID, MainFrame::OnButtonClicked)
 wxEND_EVENT_TABLE()
 
 
-
 MainFrame::MainFrame(const wxString& title) :wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 	wxStaticBox* groupboxPass = new wxStaticBox(panel, wxID_ANY, "Login Panel", wxPoint(20, 10), wxSize(381, 551));
 	wxButton* loginButton = new wxButton(panel, BUTTON_ID, "Login", wxPoint(48, 341), wxSize(321, 41));
+	//wxStaticText* userText = new wxStaticText(panel, wxID_ANY, wxPoint(48, 100), wxDefaultSize, );
+	//wxStaticText* passText = new wxStaticText(panel, wxID_ANY, wxPoint(48, 100), wxDefaultSize, );.
+	//wxStaticText* linker = new wxStaticText(panel, wxID_ANY, wxPoint(48, 100), wxDefaultSize, "Don't have an account? Register now.");
 	wxTextCtrl* inputUser = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxPoint(48, 120), wxSize(321, 41));
 	wxTextCtrl* inputPass = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, wxPoint(48, 210), wxSize(321, 41));
-	//wxStaticText* userText = new wxStaticText(panel, wxID_ANY, wxPoint(48, 100), wxDefaultSize, );
-	//wxStaticText* passText = new wxStaticText(panel, wxID_ANY, wxPoint(48, 100), wxDefaultSize, );
 	CreateStatusBar();
 }
 
 void MainFrame::OnButtonClicked(wxCommandEvent& event) {
+
+	wxString username = inputUser->GetValue();
+	wxString password = inputPassword->GetValue();
+
 	wxLogStatus("verifying..");	
 
+	try {
+		CURLcode login = MainFrame::login(username, password);
+		if(login == CURLE_OK){
+			wxLogStatus("Logged");
+			MainFrame* mainPanel = new MainFrame("Menu");
+			mainPanel->Show(true);
+		}
+		else {
+			std::cout << "error";
+			wxLogStatus("Bad Request");
+		}
+	}
+	catch(...){
+		wxLogStatus("An error occurred. Restart the program");
+	}
 }
--- Password Generator/MainFrame.h
+++ Password Generator/MainFrame.h
@@ -1,10 +1,84 @@
 #pragma once
+#include <curl/curl.h>
+#include <json/json.h>
+#include "MainFrame.h"
+#include <string>
 #include <wx/wx.h>
+#include <random>
+#include <ctime>
 
 class MainFrame : public wxFrame {
 public:
 	MainFrame(const wxString& title);
 private:
+
+    wxTextCtrl* inputUser; 
+    wxTextCtrl* inputPassword;
+    
+    // main widgets
+
+    wxTextCtrl* pswLen;
+
+    wxCheckBox uppercases;
+    wxCheckBox symbols;
+    wxCheckBox numbers;
+
 	void OnButtonClicked(wxCommandEvent& event);
 	wxDECLARE_EVENT_TABLE();
+
+    CURLcode login(wxString& inputUsername, wxString& inputPassword) {
+        CURL* curl = curl_easy_init();
+        CURLcode statusCode;
+        Json::Value data;
+        data["username"] = inputUsername; // inputUsername = string ( json requires strings )
+        data["password"] = inputPassword; // inputPassword = string ( json requires strings )
+        Json::FastWriter writer;
+        std::string jsonPayload = writer.write(data);
+
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "login.php");
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonPayload.c_str());
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER,
+                curl_slist_append(NULL, "Content-Type: application/json"));
+            statusCode = curl_easy_perform(curl);
+            if (statusCode != CURLE_OK) {
+                return statusCode; //debugging statement
+            }
+            else
+            {
+                return CURLE_OK;
+            }
+            curl_easy_cleanup(curl);
+        }
+    }
+
+    void generatePassword() {
+
+        std::string lowercases = "abcdefghijklmnopqrstuvwxyz";
+        std::string uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        std::string symbols = "!£$%&/()=?^*";
+        std::string numbers = "0123456789";
+        std::string completePass = lowercases;
+
+        int len = sizeof(completePass) - 1;
+
+        srand(time(0));
+
+        if () // checkbox conditions
+        {
+            completePass += uppercases;
+        }
+        if ()
+        {
+            completePass += symbols;
+        }
+        if ()
+        {
+            completePass += numbers;
+        }
+
+        for (int i = 0; i < len; i++) {
+            completePass = completePass[rand() % len];
+        }
+    }
 };
